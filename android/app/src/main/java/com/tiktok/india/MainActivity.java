30fe0454ee524aecee9164739474d4308d0ed385
surajkumarmotihari77-oss <surajkumarmotihari77@gmail.com>
2025-08-31 14:44:55 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/tiktok/india/MainActivity.java
+++ android/app/src/main/java/com/tiktok/india/MainActivity.java
@@ -1,11 +1,6 @@
 package com.tiktok.india;
 
-import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 }
