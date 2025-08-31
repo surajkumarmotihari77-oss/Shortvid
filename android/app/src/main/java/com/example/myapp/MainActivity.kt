02464ac23f79f385172bf594feeeb72cc5d4326a
surajkumarmotihari77-oss <surajkumarmotihari77@gmail.com>
2025-08-31 12:45:19 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/example/myapp/MainActivity.kt
+++ android/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.tik.tok.india
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity: FlutterActivity() {
 }
